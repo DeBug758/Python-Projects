6e823b46f1617f4f14b45e558d372c89a14127a8
DeBug758 <94057435+DeBug758@users.noreply.github.com>
2024-02-08 15:31:59 +0600

    Create pull_requests.py

---
+++ flask-app-docker/handlers/pull_requests.py
@@ -0,0 +1,26 @@
+from flask import Flask, jsonify
+import os
+
+import requests
+
+TOKEN = os.getenv("TOKEN")
+HEADERS = {'Authorization': f'Bearer {TOKEN}'}
+
+
+def get_pull_requests(state=None):
+    response = requests.get("https://api.github.com/repos/boto/boto3/pulls", headers=HEADERS,
+                            params={'state': state, 'per_page': 100})
+    if response.status_code == 200:
+        pulls = response.json()
+        reduced_info = []
+        for pr in pulls:
+            pr_info = {
+                'title': pr['title'],
+                'num': pr['number'],
+                'link': pr['html_url'],
+            }
+            reduced_info.append(pr_info)
+        return reduced_info
+    else:
+        return jsonify({"error": "Failed to fetch data from GitHub"}), 500
+
